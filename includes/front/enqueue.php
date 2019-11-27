cf4b5af300acc88bab3da4a6dfe334b5ca8dfed0
Diego <dyeguitto@hotmail.com>
2019-11-27 16:38:28 -0300

    Registering styles

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -1,1 +1,16 @@
 <?php
+
+
+function ju_enqueue(){
+    $uri                =   get_theme_file_uri();
+
+    wp_register_style( 'ju_google_fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i' );
+    wp_register_style( 'ju_bootstrap', $uri . '/assets/css/bootstrap.css' );
+    wp_register_style( 'ju_style', $uri . '/assets/css/style.css' );
+    wp_register_style( 'ju_dark', $uri . '/assets/css/dark.css' );
+    wp_register_style( 'ju_font_icons', $uri . '/assets/css/font-icons.css' );
+    wp_register_style( 'ju_animate', $uri . '/assets/css/animate.css' );
+    wp_register_style( 'ju_magnific_popup', $uri . '/assets/css/magnific-popup.css' );
+    wp_register_style( 'ju_responsive', $uri . '/assets/css/responsive.css' );
+    wp_register_style( 'ju_custom', $uri . '/assets/css/custom.css' );
+}
--- index.php
+++ index.php
@@ -8,10 +8,10 @@
   <!-- Stylesheets
 	============================================= -->
   <link
-    href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i"
+    href=""
     rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <link rel="stylesheet" href="" type="text/css" />
   <link rel="stylesheet" href="css/dark.css" type="text/css" />
   <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
   <link rel="stylesheet" href="css/animate.css" type="text/css" />
