fbb1acf369eeab1c94b49e9410aa5a2705043742
Diego Mandu <dyeguitto@hotmail.com>
2019-12-19 16:52:26 -0300

    Insertin HTML

--- 404.php
+++ 404.php
@@ -1,4 +1,47 @@
-
 <?php get_header(); ?>
+
+    <!-- Page Title
+    ============================================= -->
+    <section id="page-title">
+      <div class="container clearfix">
+        <h1>Page Not Found</h1>
+      </div>
+    </section><!-- #page-title end -->
+
+    <!-- Content
+    ============================================= -->
+    <section id="content">
+
+      <div class="content-wrap">
+
+        <div class="container clearfix">
+
+          <div class="col_half nobottommargin">
+            <div class="error404 center">404</div>
+          </div>
+
+          <div class="col_half nobottommargin col_last">
+
+            <div class="heading-block nobottomborder">
+              <h4>Ooopps! The Page you were looking for, couldn't be found.</h4>
+              <span>Try searching for the best match or browse the links below:</span>
+            </div>
+
+            <form action="#" method="get" role="form" class="nobottommargin">
+              <div class="input-group input-group-lg">
+                <input type="text" class="form-control"
+                                       placeholder="Search for Pages...">
+                <span class="input-group-btn"><button class="btn btn-danger" type="button"><i class="icon-search"></i></button></span>
+              </div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section><!-- #content end -->
+
 
 <?php get_footer(); ?>
