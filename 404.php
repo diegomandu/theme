89a4690191b902d6d767c21112306a13c5ece303
Diego Mandu <dyeguitto@hotmail.com>
2019-12-19 17:05:37 -0300

    Making content translatable

--- 404.php
+++ 404.php
@@ -1,47 +1,41 @@
 <?php get_header(); ?>
 
-    <!-- Page Title
-    ============================================= -->
-    <section id="page-title">
+<!-- Page Title
+  ============================================= -->
+  <section id="page-title">
+    <div class="container clearfix">
+      <h1><?php _e( "Page Not Found", 'udemy' ); ?></h1>
+    </div>
+  </section><!-- #page-title end -->
+
+<!-- Content
+  ============================================= -->
+  <section id="content">
+
+    <div class="content-wrap">
+
       <div class="container clearfix">
-        <h1>Page Not Found</h1>
-      </div>
-    </section><!-- #page-title end -->
 
-    <!-- Content
-    ============================================= -->
-    <section id="content">
+        <div class="col_half nobottommargin">
+          <div class="error404 center">404</div>
+        </div>
 
-      <div class="content-wrap">
+        <div class="col_half nobottommargin col_last">
 
-        <div class="container clearfix">
-
-          <div class="col_half nobottommargin">
-            <div class="error404 center">404</div>
+          <div class="heading-block nobottomborder">
+            <h4><?php _e( "Ooopps! The Page you were looking for, couldn't be found.", 'udemy' ); ?></h4>
+            <span><?php _e( "Try searching for the best match or browse the links below:", 'udemy' ); ?></span>
           </div>
 
-          <div class="col_half nobottommargin col_last">
-
-            <div class="heading-block nobottomborder">
-              <h4>Ooopps! The Page you were looking for, couldn't be found.</h4>
-              <span>Try searching for the best match or browse the links below:</span>
-            </div>
-
-            <form action="#" method="get" role="form" class="nobottommargin">
-              <div class="input-group input-group-lg">
-                <input type="text" class="form-control"
-                                       placeholder="Search for Pages...">
-                <span class="input-group-btn"><button class="btn btn-danger" type="button"><i class="icon-search"></i></button></span>
-              </div>
-            </form>
-
-          </div>
+          <?php get_search_form(); ?>
 
         </div>
 
       </div>
 
-    </section><!-- #content end -->
+    </div>
+
+  </section><!-- #content end -->
 
 
 <?php get_footer(); ?>
