ca9f7b893e5ec5887361799cfb0338ffbebdfd1b
Diego Mandu <dyeguitto@hotmail.com>
2019-12-19 15:21:25 -0300

    Checking if has thumbnail before displaying it

--- single.php
+++ single.php
@@ -165,16 +165,29 @@
                   ?>
 
                     <div class="mpost clearfix">
-                      <div class="entry-image">
-                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-                      </div>
+                      <?php 
+
+                      if( has_post_thumbnail() ){
+                      ?>
+
+                        <div class="entry-image">
+                          <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail( 'thumbnail' ); ?>    
+                          </a>
+                        </div>
+
+                          <?php
+                      }
+
+                           ?>
+                      
                     <div class="entry-c">
                       <div class="entry-title">
                         <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
                       </div>
                       <ul class="entry-meta clearfix">
-                        <li><i class="icon-calendar3"></i><?php the_date(); ?></li>
-                        <li><i class="icon-comments"></i><?php comments_number(); ?></li>
+                        <li><i class="icon-calendar3"></i><?php echo get_the_date(); ?></li>
+                        <li><i class="icon-comments"></i><?php comments_number( '0' ); ?></li>
                       </ul>
                       <div class="entry-content"><?php the_excerpt(); ?></div>
                     </div>
