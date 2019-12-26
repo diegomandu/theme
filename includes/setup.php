3d301a436c72612a901800482e6654e3f120e70e
Diego Mandu <dyeguitto@hotmail.com>
2019-12-26 15:42:02 -0300

    Add custom logo support

--- includes/setup.php
+++ includes/setup.php
@@ -3,6 +3,7 @@
 function ju_setup_theme(){
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'title-tag' );
+    add_theme_support( 'custom-logo' );
     
     register_nav_menu( 'primary', __( 'Primary Menu', 'udemy' ) );
 }
