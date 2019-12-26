b1a92de8cf479d52a0c395410ed2d5206bc542ec
Diego Mandu <dyeguitto@hotmail.com>
2019-12-26 19:02:31 -0300

    Creating a new folder and file to organize the customizer logic

---
+++ includes/customizer/social.php
@@ -0,0 +1,23 @@
+<?php 
+
+function ju_social_customizer_section( $wp_customize ){
+	$wp_customize->add_setting( 'ju_facebook_handle',[
+		'default'	=>	''
+	]);
+
+	$wp_customize->add_section( 'ju_social_section', [
+		'title'		=>	__( 'Udemy Social Settings', 'udemy' ),
+		'priority'	=>	30
+	]);
+
+	$wp_customize->add_control(new WP_Customize_Control(
+		$wp_customize,
+		'ju_social_facebook_input',
+		array(
+			'label'			=>	__( 'Facebook Handle', 'udemy' ),
+			'section'		=>	'ju_social_section',
+			'settings'		=>	'ju_facebook_handle'
+		)
+	));
+
+}
--- includes/theme-customizer.php
+++ includes/theme-customizer.php
@@ -1,23 +1,5 @@
 <?php 
 
 function ju_customize_register( $wp_customize ){
-	$wp_customize->add_setting( 'ju_facebook_handle',[
-		'default'	=>	''
-	]);
-
-	$wp_customize->add_section( 'ju_social_section', [
-		'title'		=>	__( 'Udemy Social Settings', 'udemy' ),
-		'priority'	=>	30
-	]);
-
-	$wp_customize->add_control(new WP_Customize_Control(
-		$wp_customize,
-		'ju_social_facebook_input',
-		array(
-			'label'			=>	__( 'Facebook Handle', 'udemy' ),
-			'section'		=>	'ju_social_section',
-			'settings'		=>	'ju_facebook_handle'
-		)
-	));
-
+	ju_social_customizer_section( $wp_customize );
 }
