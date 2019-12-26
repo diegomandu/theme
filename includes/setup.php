ab27decbdb99a214e945a7ce6e99b94b83221389
Diego Mandu <dyeguitto@hotmail.com>
2019-12-26 15:40:52 -0300

    Add theme support title tag

--- includes/setup.php
+++ includes/setup.php
@@ -2,6 +2,7 @@
 
 function ju_setup_theme(){
     add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'title-tag' );
     
     register_nav_menu( 'primary', __( 'Primary Menu', 'udemy' ) );
 }
