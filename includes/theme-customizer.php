ea1b05d7461ce876dfacb165f9a1510b6b2fa4ef
Diego Mandu <dyeguitto@hotmail.com>
2019-12-31 18:28:35 -0300

    Changing the one of the default label from Customizer

--- includes/theme-customizer.php
+++ includes/theme-customizer.php
@@ -1,12 +1,21 @@
+
+
 <?php 
 
 function ju_customize_register( $wp_customize ){
+	// echo '<pre>';
+	// var_dump( $wp_customize );
+	// echo '</pre>';
+
+	$wp_customize->get_section( 'title_tagline' )->title 	=	'General';
+
+
 	$wp_customize->add_panel( 'udemy', [
 		'title'			=>	__( 'Udemy', 'udemy' ),
 		'descriptions'	=>	'<p>Udemy Theme Settings</p>',
 		'priority'		=>	160
 	]);
-	
+
 	ju_social_customizer_section( $wp_customize );
 	ju_misc_customizer_section( $wp_customize );
 }
