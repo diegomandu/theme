1f032153b1133103d186a57cf5a53ebbb5f9900e
Diego Mandu <dyeguitto@hotmail.com>
2019-12-31 16:18:46 -0300

    Add settings for misc customizer

--- includes/customizer/misc.php
+++ includes/customizer/misc.php
@@ -1,6 +1,24 @@
 <?php 
 
 function ju_misc_customizer_section( $wp_customize ){
+	$wp_customize->add_setting( 'ju_header_show_search', [
+		'default'		=>	'yes'
+	]);
 
+	$wp_customize->add_setting( 'ju_header_show_cart', array(
+		'default'		=>	'yes',
+	));
+
+	$wp_customize->add_setting( 'ju_footer_copyright_text', array(
+		'default'		=>	'Copyrights &copy; 2019 All Rights Reserved.',
+	));
+
+	$wp_customize->add_setting( 'ju_footer_tos_page', array(
+		'default'		=>	0,
+	));
+
+	$wp_customize->add_setting( 'ju_footer_privacy_page', array(
+		'default'		=>	0,
+	));
 
 }
