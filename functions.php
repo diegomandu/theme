7a50d5a4d91e84f5cae57764e4868d58b3ca534c
Diego Mandu <dyeguitto@hotmail.com>
2019-12-31 18:34:58 -0300

    add action to just load js file when opening customize

--- functions.php
+++ functions.php
@@ -19,6 +19,8 @@ add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
 add_action( 'after_setup_theme', 'ju_setup_theme' );
 add_action( 'widgets_init', 'ju_widgets' );
 add_action( 'customize_register', 'ju_customize_register' );
+add_action( 'customize_preview_init', 'ju_customize_preview_init' );
+
 
 
 // Shortcodes
