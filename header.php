d5c91cc76516e944fc2273e806ed212b8feb8403
Diego Mandu <dyeguitto@hotmail.com>
2020-01-01 16:09:45 -0300

    Add the bloginfo charset

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
 
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta http-equiv="content-type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
 
   <!-- Stylesheets
 	============================================= -->
