2248d83113b16e7164bd8c3946a05e8e30742709
Diego Mandu <dyeguitto@hotmail.com>
2020-01-26 15:33:05 -0300

    Define function ju_buddypress_posts_content

--- includes/buddypress/profile-posts.php
+++ includes/buddypress/profile-posts.php
@@ -11,3 +11,35 @@ function ju_recent_recipes_tabs(){
 function ju_buddypress_recent_posts_title(){
 	?><div class="text-center">My Recipes</div><?php
 }
+
+function ju_buddypress_posts_content(){
+	$profile_user_id			=	bp_displayed_user_id();
+
+	if( !$profile_user_id ){
+		return;
+	}
+
+	$posts 						=	new WP_Query([
+		'author'				=>	$profile_user_idm,
+		'posts_per_page'		=>	10,
+		'post_type'				=>	'recipe'
+	]);
+
+	if( $posts->have_posts() ){
+	  ?>
+	  <div id="posts" class="row justify-content-md-center">
+	  	<div class="col-md-8">
+	  	<?php
+	  	while( $posts->have_posts() ){
+	  		$posts->the_post();
+
+	  		get_template_part( 'partials/post/content-excerpt' );
+	  	}
+	  	?>
+	  	</div>
+	  </div>
+	  <?php
+
+	  wp_reset_postdata();
+	}
+}
--- includes/buddypress/profile-tabs.php
+++ includes/buddypress/profile-tabs.php
@@ -8,7 +8,7 @@ function ju_buddypress_profile_tabs(){
 	global $bp;
 
 	bp_core_new_nav_item([
-		'name'						=>	esc_html__( 'Recipes', 'udemy' );
+		'name'						=>	esc_html__( 'Recipes', 'udemy' ),
 		'slug'						=>	'recipes',
 		'position'					=>	100,
 		'screen_function'			=>	'ju_recent_recipes_tab',
