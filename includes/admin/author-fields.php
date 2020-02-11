abb4e883c095e2890cca2dc2b8b74f9e8c1d3a1f
Diego Mandu <dyeguitto@hotmail.com>
2020-02-11 13:21:20 -0300

    Define function ju_custom_user_profile_fields

--- includes/admin/author-fields.php
+++ includes/admin/author-fields.php
@@ -1,5 +1,16 @@
 <?php 
 
 function ju_custom_user_profile_fields(){
-	
+	?>
+	<table class="form-table">
+		<tr>
+			<th>
+				<label for="ju_twitter"><?php _e( 'Twitter', 'udemy' ); ?></label>
+			</th>
+			<td>
+				<input type="text" name="ju_twitter" id="ju_twitter" class="regular-text"
+						value="<?php echo esc_attr( get_the_author_meta( 'ju_twitter', $user->ID ); ) ?>"/>
+			</td>
+		</tr>
+	</table>
 }
